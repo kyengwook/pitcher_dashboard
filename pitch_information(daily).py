c517f3e873691674f34d5064a4e5f89ca58e3159
김경욱 - Kyengwook, Kim <93772960+kyengwook@users.noreply.github.com>
2025-05-11 07:08:10 +0900

    Update pitch_information(daily).py

--- pitch_information(daily).py
+++ pitch_information(daily).py
@@ -10,13 +10,11 @@ st.set_page_config(layout="wide")
 #데이터 로드
 @st.cache_data
 def load_data_from_drive():
-    file_id = "1sWJCEA7MUrOCGfj61ES1JQHJGBfYVYN3"  # 본인의 파일 ID
-    download_url = f"https://drive.google.com/uc?export=download&id={file_id}"
-    response = requests.get(download_url)
-    response.raise_for_status()
-    
-    df = pd.read_csv(io.StringIO(response.content.decode("utf-8")), encoding='utf-8')
-    df = df[df['game_type'] == 'R']  # 정규시즌만
+    url = 'https://drive.google.com/uc?id=1vZB9axWHpzUB5ixNG9Q3JtxTxQsCDMD4'
+    output = 'data.csv'
+    gdown.download(url, output, quiet=False)
+    df = pd.read_csv(output)
+    df = df[df['game_type'] == 'R']
     df['game_date'] = pd.to_datetime(df['game_date'])
     df = df.set_index('game_date').sort_index()
     return df
